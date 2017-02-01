d14b58cffa0b5a1ddc27d05338b79c786bda1263
Henry Post <HenryFBP@gmail.com>
2017-01-31 20:28:46 -0600

    added comment for how to spawn creep

--- tutorial-1/main.js
+++ tutorial-1/main.js
@@ -1,3 +1,28 @@
-module.exports.loop = function () {
+//Game.spawns['Spawn1'].createCreep( [WORK, CARRY, MOVE], 'Harvester1' );
+
+
+
+module.exports.loop = function ()
+{
+
+    var creep = Game.creeps['Harvester1'];
+
+    if(creep.carry.energy < creep.carryCapacity)
+    {//we're not full yet
+        var sources = creep.room.find(FIND_SOURCES);
+
+        if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE)
+        {
+            creep.moveTo(sources[0]);
+        }
+    }
+    else
+    {//we're full
+        if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
+        {//goto base
+            creep.moveTo(Game.spawns['Spawn1']);
+        }
+    }
+
 
 }
