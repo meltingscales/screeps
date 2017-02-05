07b632cef690bcb3016fbc92335746e90ac2f718
Henry Post <HenryFBP@gmail.com>
2017-02-05 14:52:07 -0600

    thxc frie lalamers

--- tutorial-1/spawn.autobirth.js
+++ tutorial-1/spawn.autobirth.js
@@ -1,75 +1,74 @@
-var _ = require('helperfns');
+var helperfns = require('helperfns');
 
-var refresh = 3;
+var refresh = 7;
 
 
 
 /**
   * @param {StructureSpawn} spawner
-  * @param {list_maybe} maxPop
-  * @param {gist_maybe} ratiosList
-  * @param {fist_maybe} bodypartsList
-  * @param {string} nameTemplate
+  * @param {integer} maxPop
+  * @param {integer[]} ratiosList
+  * @param {integer[]} bodypartsList
+  * @param {string[]} nameTemplate
   *
   **/
 function _maintainPopulation(spawner, maxPop, ratiosList, bodypartsList, nameTemplate)
 {
-  console.log("MaintainPop");
-  console.log("Was passed " + spawner + ", " + maxPop + ", " + ratiosList + ", " + bodypartsList + ", " + nameTemplate);
-  // console.log("spawner's spawned creeps = " + Memory.spawns[spawner.name].creeps.length );
-
-  var currPop = Memory.spawns[spawner.name].creeps.length;
-  var bodyTypesList; //a mathemagical combo of (ratios of bodytypes) and (max pop)
-
-  //we have [
-  //         [WORK,CARRY,MOVE],
-  //         [CARRY,CARRY,MOVE],
-  //         [WORK,WORK,MOVE]
-  //        ]
-  //
-  //  and have [0.6, 0.1, 0.3]
-  //
-  //  AND have 10.
-  //
-  //  how do we construct a proportional list of WHOLE numbers?
-  //
-  //  We can try multiplying it directly. That gets us:
-  //  [6, 1, 3], which is valid. Does it work for uglier requests?
-  //
-  //  we want 11.
-  //
-  //  [6.6, 1.1, 3.3] is invalid...but, I see a pattern here... we round them.
-  //  [7,   1,   3]
-  //
-  //
-  // one last case. equal numbers, which gets priority?
-  //
-  //
-  // [0, 0.5, 0.5] with an 11.
-  //
-  // [0, 5.5, 5.5]
-  // [0, 6,   6]  this is bad. we've got one too many. we'll deal with this later.
-  //
 
   if(currPop > maxPop)
   {
     console.log(currPop + " > " + maxPop + ", so not spawning...");//we dont need to spawn
+    return;
   }
-
-
-
-
-
   else
   {
     //now we need to determine what to spawn...
 
-    //loop through all screeps tied to this spawn, subtracting the "existing bodies" from our bodyTypesList
-      //
-      //
-    //
+
+      console.log("\n\nMaintainPop");
+      console.log("Was passed " + spawner + ", " + maxPop + ", " + ratiosList + ", " + JSON.stringify(bodypartsList) + ", " + nameTemplate);
+      // console.log("spawner's spawned creeps = " + Memory.spawns[spawner.name].creeps.length );
+
+      if(typeof(Memory.spawns[spawner.name]) == 'undefined')
+      {
+        Memory.spawns[spawner.name] = {}; //make memory.spawns list
+        Memory.spawns[spawner.name].creeps = []; //make memory.spawns[spawnerName] list
+        console.log("Memory.spawns." + spawner.name + "was undefined, so we made a new OBJECT.");
+      }
+
+
+      var currPop = Memory.spawns[spawner.name].creeps.length;
+
+      var bodyTypesList = ratiosList; //a mathemagical combo of (ratios of bodytypes) and (max pop)
+
+      for(i = 0; i < bodyTypesList.length; i++)
+      {
+        bodyTypesList[i] = Math.round(bodyTypesList[i] * maxPop);
+
+        console.log("We want " + bodyTypesList[i] + " of bodytype \'" + bodypartsList[i] + "\', not factoring in preexisting workers");
+      }
+
+      
+
+
+      console.log(bodyTypesList);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   }
 
+  console.log("\n\n");
 
 }
 
@@ -78,16 +77,16 @@ var poobyputt =
 {
 
 /**
-  * @param {Game.spawns} spawner
-  * @param {list_maybe} maxPop
-  * @param {gist_maybe} ratiosList
-  * @param {fist_maybe} bodypartsList
-  * @param {string} nameTemplate
+  * @param {StructureSpawn} spawner
+  * @param {integer} maxPop
+  * @param {integer[]} ratiosList
+  * @param {integer[]} bodypartsList
+  * @param {string[]} nameTemplate
   *
   **/
   maintainPopulation: function(spawner, maxPop, ratiosList, bodypartsList, nameTemplate)
   {
-    _.runEvery(function(){_maintainPopulation(spawner,maxPop,ratiosList,bodypartsList,nameTemplate)}, refresh); //ANON FUNC BRUH
+    helperfns.runEvery(function(){_maintainPopulation(spawner,maxPop,ratiosList,bodypartsList,nameTemplate)}, refresh); //ANON FUNC BRUH
   },
 
 
